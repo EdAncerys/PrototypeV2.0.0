fac5e1ed8b1cd573ff6df7348444f71b0f0fb701
Ed <lookatemail@gmail.com>
2020-12-29 15:15:43 +0000

    Add serverless - exporting app and wraping with serverless

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.json({ data: 'Hello World' });
 });
 
+// Allowing lambda function to run - exporting handler function
+module.exports = app;
+module.exports.handler = serverless(app);
+
 // Express app port No
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
