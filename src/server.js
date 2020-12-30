1047aee603af7c31f0ec3a19c4104aec86603d83
Ed <lookatemail@gmail.com>
2020-12-30 11:26:08 +0000

    Able to render data dynamically in DOM

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ const serverless = require('serverless-http'); // Serverless module allowing to 
 require('dotenv').config(); // Enabling to load Environment variables from a .env File
 const freshDesk = require('./freshDesk');
 
+app.set('view engine', 'ejs'); // set the view engine to ejs
 // Middleware
 app.use(freshDesk);
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ data: 'Hello World' });
+  // res.json({ data: 'Hello World' });
+  res.render('../dist/views/index.ejs', {
+    data: {
+      name: 'Frodo',
+      password: 'password',
+    },
+  });
 });
 
 // Allowing lambda function to run - exporting handler function
